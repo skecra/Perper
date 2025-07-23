96d7c3fcc876cd3dd7f4ca92a39ed356171ed40e
Skecra <radulence777@gmail.com>
2025-07-23 22:45:42 +0200

    odradjen transfer from uz mijenjanje stanja i allowanca 7

--- test/Perper.js
+++ test/Perper.js
@@ -79,6 +79,45 @@ contract('Perper', function(accounts) {
         })
     })
 
-
+    it('handles token transfers', function(){
+        return Perper.deployed().then(function(instance){
+        tokenInstance = instance;    
+        fromAccount = accounts[2]
+        toAccount = accounts[3]
+        spendingAccount = accounts[4]
+        //transfer tokens
+        return tokenInstance.transfer(fromAccount, 100, { from: accounts[0]})
+    }).then(function(receipt){
+        //approve spending account
+        return tokenInstance.approve(spendingAccount, 10, {from: fromAccount})
+    }).then(function(receipt){
+        //try transfer more than account have
+        return tokenInstance.transferFrom(fromAccount, toAccount, 9999, {from: spendingAccount})
+    }).then(assert.fail).catch(function(error){
+        assert(error.message.indexOf('revert') >= 0), 'cant transfer more than you have'
+        return tokenInstance.transferFrom(fromAccount, toAccount, 20, {from: spendingAccount})
+    }).then(assert.fail).catch(function(error){
+        assert(error.message.indexOf('revert') >= 0, 'canot transfer more than approved ammount of tokens')
+        return tokenInstance.transferFrom.call(fromAccount, toAccount, 10, {from: spendingAccount})
+    }).then(function(success){
+        assert.equal(success, true)
+        return tokenInstance.transferFrom(fromAccount, toAccount, 10, {from: spendingAccount})
+    }).then(function(receipt){
+            assert.equal(receipt.logs.length, 1, 'triggers event');
+            assert.equal(receipt.logs[0].event, 'Transfer', 'should be "Transfer" event');
+            assert.equal(receipt.logs[0].args._from, fromAccount, 'logs the account the tokens are aprovedred from');
+            assert.equal(receipt.logs[0].args._to, toAccount, 'logs the account the tokens are aprovedred to');
+            assert.equal(receipt.logs[0].args._value, 10, 'logs the aproved amount');
+            return tokenInstance.balanceOf(fromAccount)
+        }).then(function(balance){
+            assert.equal(balance.toNumber(), 90, 'deducts amout from sender')
+            return tokenInstance.balanceOf(toAccount);
+        }).then(function(balance){
+            assert.equal(balance.toNumber(), 10, 'add amout to reciever')
+            return tokenInstance.allowance(fromAccount, spendingAccount)
+        }).then(function(allowance){
+            assert.equal(allowance, 0, 'deducts the amount from allowance')
+        })
+})
 
 })
